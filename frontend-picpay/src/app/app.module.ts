02901c64fd31ffee78ea5b5927cdf5fbb88cf0de
Elias Faiçal <eliasfaical@gmail.com>
2018-03-07 23:05:04 -0300

    Creando componentes do front-end

--- frontend-picpay/src/app/app.module.ts
+++ frontend-picpay/src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { UsersComponent } from './users/users.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UsersComponent
   ],
   imports: [
+    HttpModule,
     BrowserModule
   ],
   providers: [],
