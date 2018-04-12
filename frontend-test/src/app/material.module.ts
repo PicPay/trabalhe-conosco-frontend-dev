e6e8ebfd9ee8685766b411a81b094a9bee4c2037
Victor Martins <victorfrontend@gmail.com>
2018-04-11 23:14:46 -0300

    payment: show user data in dialog and configure stepper

--- frontend-test/src/app/material.module.ts
+++ frontend-test/src/app/material.module.ts
@@ -7,7 +7,8 @@ import {
   MatLineModule,
   MatIconModule,
   MatButtonModule,
-  MatDialogModule
+  MatDialogModule,
+  MatStepperModule
 } from '@angular/material';
 
 @NgModule({
@@ -19,7 +20,8 @@ import {
     MatLineModule,
     MatIconModule,
     MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatStepperModule
   ],
   exports: [
     MatToolbarModule,
@@ -27,7 +29,8 @@ import {
     MatLineModule,
     MatIconModule,
     MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatStepperModule
   ],
   declarations: []
 })
