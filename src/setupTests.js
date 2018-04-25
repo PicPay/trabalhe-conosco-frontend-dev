0c45fe1b651b588388c32f8267d07a667074ed41
Israel <israelhenrique@protonmail.com>
2018-04-25 15:02:16 -0300

    mock localStorage

--- src/setupTests.js
+++ src/setupTests.js
@@ -4,3 +4,27 @@ import Adapter from 'enzyme-adapter-react-16'
 global.fetch = require('node-fetch')
 
 Enzyme.configure({ adapter: new Adapter() })
+
+class LocalStorageMock {
+  constructor() {
+    this.store = {}
+  }
+
+  clear() {
+    this.store = {}
+  }
+
+  getItem(key) {
+    return this.store[key] || null
+  }
+
+  setItem(key, value) {
+    this.store[key] = value
+  }
+
+  removeItem(key) {
+    delete this.store[key]
+  }
+}
+
+global.localStorage = new LocalStorageMock()
