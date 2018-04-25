14aba9a3a52389855c37b48b7f0d75bc3c592125
francismozart@gmail.com <francismozart@gmail.com>
2018-04-25 01:02:59 -0300

    cadastro, escolha de cartoes, transação acontecendo.

--- -resources/js/app.js
+++ -resources/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('testeFrontApp', ['ngRoute', 'ngAnimate', 'LocalStorageModule']);
+var app = angular.module('testeFrontApp', ['ngRoute', 'ngAnimate', 'LocalStorageModule', 'ngMask']);
 
 app.config(function($routeProvider) {
 	$routeProvider.
@@ -18,6 +18,10 @@ app.config(function($routeProvider) {
 		templateUrl: 'views/card.html',
 		controller: 'creditCardCtrl'
 	})
+	.when('/mycards', {
+		templateUrl: 'views/mycards.html',
+		controller: 'myCardsCtrl'
+	})
 });
 
 app.factory('personToPayFactory', function() {
@@ -29,6 +33,24 @@ app.factory('personToPayFactory', function() {
 
 	function get() {
 		return person;
+	}
+
+	return {
+		set: set,
+		get: get
+	}
+});
+
+app.factory('creditCardsFactory', function(localStorageService) {
+	var creditCards = JSON.parse(localStorage.getItem("creditCards") || "[]");
+
+	function set(data) {
+		creditCards.push(data);
+		localStorage.setItem("creditCards", JSON.stringify(creditCards));
+	}
+
+	function get() {
+		return JSON.parse(localStorage.getItem("creditCards") || "[]");
 	}
 
 	return {
@@ -55,30 +77,87 @@ app.controller('listCtrl', function($scope, $location, $http, personToPayFactory
 	}
 });
 
-app.controller('paymentCtrl', function($scope, personToPayFactory, localStorageService){
+app.controller('paymentCtrl', function($scope, personToPayFactory, $http, creditCardsFactory){
 	$scope.personToPay = personToPayFactory.get();
-	$scope.creditCards = [];
+	$scope.creditCards = creditCardsFactory.get();
+	$scope.selectedCard = null;
+	$scope.lastDigits = 0;
+	$scope.value = "";
+	$scope.mountedObjectToPay = {};
 
-	$scope.creditCards = localStorageService.get('creditCards');
-	console.log($scope.creditCards);
+	angular.forEach($scope.creditCards, function(value, key) {
+		if (value.is_selected) {
+			$scope.selectedCard = value;
+			$scope.lastDigits = $scope.selectedCard.card_number.substr($scope.selectedCard.card_number.length - 4);
+		}
+	});
+
+	$scope.pay = function() {
+		$scope.mountedObjectToPay = {
+			card_number: $scope.selectedCard.card_number.replace(/\s+/g,''),
+			cvv: $scope.selectedCard.ccv,
+			value: $scope.value,
+			expiry_date: $scope.selectedCard.expiry_date,
+			destination_user_id: $scope.personToPay.id
+		}
+
+		$http({
+			method: 'POST',
+			url: 'http://careers.picpay.com/tests/mobdev/transaction',
+			data: $scope.mountedObjectToPay
+		}).then(function successCallback(response) {
+			console.log(response);
+		}, function errorCallback(response) {
+			$scope.people = response.statusText;
+		});
+	}
 });
 
-app.controller('creditCardCtrl', function($scope, localStorageService){
+app.controller('creditCardCtrl', function($scope, creditCardsFactory, $route){
 	$scope.creditCard = {
 		card_name: "",
 		card_number:"",
-		cvv:0,
+		cvv: null,
 		expiry_date:"",
-		zip:""
+		zip:"",
+		is_selected: false
 	};
 
 	$scope.saveCard = function() {
-		localStorageService.set('creditCards', $scope.creditCard);
+		creditCardsFactory.set($scope.creditCard);
+		$route.reload();
 	}
 
 	$scope.getCard = function() {
-		let card = localStorageService.get('creditCards');
+		let card = creditCardsFactory.get();
 		console.log(card);
+	}
+});
+
+app.controller('myCardsCtrl', function($scope, creditCardsFactory, localStorageService, $route){
+
+	$scope.creditCards = creditCardsFactory.get();
+	console.log($scope.creditCards);
+
+	$scope.targetThisCard = function(card, index) {
+		card.is_selected = true;
+		console.log(card);
+
+		angular.forEach($scope.creditCards, function(value, key) {
+			if (key != index) {
+				value.is_selected = false;
+			}
+		});
+	}
+
+	$scope.deleteThisCard = function(index) {
+		$scope.creditCards.splice(index, 1);
+		localStorage.setItem("creditCards", JSON.stringify($scope.creditCards));
+	}
+
+	$scope.chooseCard = function() {
+		localStorage.setItem("creditCards", JSON.stringify($scope.creditCards));
+		$route.reload();
 	}
 });
 
