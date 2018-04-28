f1c3dbc9dafb62418da556cc4c0d7cbc71794892
Israel Lima <israelhenrique@gmail.com>
2018-04-28 13:30:01 -0300

    add Modal component

---
+++ src/view/generics-components.test.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import { Modal, Button } from './generics-components'
+
+describe('#Modal', function () {
+  it('should render without throwing an error', function () {
+    const wrapper = shallow(<Button content="Button"/>)
+
+    expect(wrapper.text()).toMatch('Button')
+  })
+})
+
+describe('#Modal', function () {
+  it('should render without throwing an error', function () {
+    const modalContent = <div>Texto</div>
+    const wrapper = shallow(<Modal content={modalContent} />)
+    const buttons = wrapper.find(Button)
+
+    expect(wrapper.text()).toMatch('Texto')
+    expect(buttons).toHaveLength(1)
+  })
+})
