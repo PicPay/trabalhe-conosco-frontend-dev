339494b93de97f511e650ba3d61952f1fc8148ba
ArthurCoimbra <arthurplank22@gmail.com>
2018-05-30 17:43:25 -0300

    Primeiro commit com os arquivos básicos do React, pasta de imagens e as outras pastas necessárias para o projeto.

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+
+class App extends Component{
+
+
+render(){
+    return(
+        <div>
+        </div>
+      )
+    }
+}
+
+ReactDOM.render(<App />,document.getElementById('root'));
