529fdad07184b7930c97b6c77f57bace54c5b497
ArthurCoimbra <arthurplank22@gmail.com>
2018-06-05 16:14:57 -0300

    Adicionada função em modal cadastro cartao para o armazenamento de dados de cartão no navegador.

--- src/components/Modal_Cadastro_Cartao.js
+++ src/components/Modal_Cadastro_Cartao.js
@@ -7,6 +7,11 @@ class ModalCadastroCartao extends Component {
     super(props);
 
     this.state={
+        bandeira: null,
+        nomeC: null,
+        validade: null,
+        codigo: null,
+        CEP: null,
         numero: '',
         isHiddenNome: true,
         isHiddenNumero: true,
@@ -15,28 +20,58 @@ class ModalCadastroCartao extends Component {
         isHiddenCEP: true
       };
 
-    this.handleClick = this.handleClick.bind(this);
     }
 
-
-  handleClick(){
-      this.props.onClose();
+//Função para lidar com o clique no botão "Cadastrar"
+  handleClick = () => {
+      this.armazenaCartao();
   }
+
+
+//Função para armazenar os dados de cartões cadastrados nas variáveis locais(cookies), máximo de 3 cartões
+  armazenaCartao = () =>{
+    if (localStorage.getItem(this.props.nome) === null) {
+        localStorage.setItem(this.props.nome, this.state.bandeira+','+this.state.nomeC+','+this.state.validade+','+
+        this.state.codigo+','+this.state.CEP+','+this.state.numero+',');
+        //console.log(localStorage.getItem(this.props.nome));
+    } else {
+      var teste = localStorage.getItem(this.props.nome);
+      var array = teste.split(',');
+      if(array.length > 6 && array.length < 11){
+          array.splice(6,1);
+          var aux = this.state.bandeira+','+this.state.nomeC+','+this.state.validade+','+
+          this.state.codigo+','+this.state.CEP+','+this.state.numero+',';
+          array.push(aux);
+          localStorage.setItem(this.props.nome, array);
+          //console.log(localStorage.getItem(this.props.nome));
+      } else if(array.length > 11 && array.length < 17){
+          array.splice(12,1);
+          aux = this.state.bandeira+','+this.state.nomeC+','+this.state.validade+','+
+          this.state.codigo+','+this.state.CEP+','+this.state.numero;
+          array.push(aux);
+          localStorage.setItem(this.props.nome, array);
+          //console.log(localStorage.getItem(this.props.nome));
+      } else {
+          console.log("numero ultrapassado");
+      }
+    }
+  }
+
 
 //Funções para mostrar/apagar as legendas no cadastro
-  toggleHiddenNome () {
+  toggleHiddenNome = () => {
       this.setState({isHiddenNome: !this.state.isHiddenNome});
   }
-  toggleHiddenNumero () {
+  toggleHiddenNumero = () => {
       this.setState({isHiddenNumero: !this.state.isHiddenNumero});
   }
-  toggleHiddenValidade () {
+  toggleHiddenValidade = () => {
       this.setState({isHiddenValidade: !this.state.isHiddenValidade});
   }
-  toggleHiddenCodigo () {
+  toggleHiddenCodigo = () => {
       this.setState({isHiddenCodigo: !this.state.isHiddenCodigo});
   }
-  toggleHiddenCEP () {
+  toggleHiddenCEP = () => {
       this.setState({isHiddenCEP: !this.state.isHiddenCEP});
   }
 
@@ -63,7 +98,7 @@ class ModalCadastroCartao extends Component {
           </div>
 
           <div className="containerCadastro">
-            <select name="cartao" className="Field-Label">
+            <select name="cartao" className="Field-Label" onChange={event => this.setState({bandeira: event.target.value})}>
               <option value="Selecione a bandeira" disabled selected hidden>Selecione a bandeira</option>
               <option value="Mastercard">Mastercard</option>
               <option value="Visa">Visa</option>
@@ -75,33 +110,37 @@ class ModalCadastroCartao extends Component {
 
           <div className="containerCadastro">
             {!this.state.isHiddenNome && <ChildNome />}
-            <input type="text" className="Field-Label" placeholder="Nome escrito no cartão" onClick={this.toggleHiddenNome.bind(this)} />
+            <input type="text" className="Field-Label" placeholder="Nome escrito no cartão"
+            onChange={event => this.setState({nomeC: event.target.value})} onClick={this.toggleHiddenNome} />
             <div className="line"></div>
           </div>
 
           <div className="containerCadastro">
             {!this.state.isHiddenNumero && <ChildNumero />}
             <input type="text" className="numeroCartao" placeholder="Número do cartão"
-             maxLength="16" onClick={this.toggleHiddenNumero.bind(this)}
+             maxLength="16" onClick={this.toggleHiddenNumero}
              onChange={event => this.setState({numero: event.target.value})} />
             <div className="line2"></div>
           </div>
 
           <div className="containerCadastro">
             {!this.state.isHiddenValidade && <ChildValidade />}
-            <input type="text" className="Field-Label" placeholder="Validade(mm/aaaa)" onClick={this.toggleHiddenValidade.bind(this)}/>
+            <input type="text" className="Field-Label" placeholder="Validade(mm/aaaa)"
+            onChange={event => this.setState({validade: event.target.value})} onClick={this.toggleHiddenValidade}/>
             <div className="line"></div>
           </div>
 
           <div className="containerCadastro">
             {!this.state.isHiddenCodigo && <ChildCodigo />}
-            <input type="text" className="Field-Label" placeholder="Código de segurança" maxLength="3" onClick={this.toggleHiddenCodigo.bind(this)}/>
+            <input type="text" className="Field-Label" placeholder="Código de segurança" maxLength="3"
+            onChange={event => this.setState({codigo: event.target.value})} onClick={this.toggleHiddenCodigo}/>
             <div className="line"></div>
           </div>
 
           <div className="containerCadastro">
             {!this.state.isHiddenCEP && <ChildCEP />}
-            <input type="text" className="Field-Label" placeholder="CEP do endereço da fatura" onClick={this.toggleHiddenCEP.bind(this)}/>
+            <input type="text" className="Field-Label" placeholder="CEP do endereço da fatura"
+            onChange={event => this.setState({CEP: event.target.value})} onClick={this.toggleHiddenCEP}/>
             <div className="line"></div>
           </div>
 
