4240348ff21a812def1f82b340e95e351bd92f7d
Everton Boni <evertonferreira.de@gmail.com>
2018-06-19 19:15:28 -0300

    add fetch api for getAll contacts/users and list on ContactList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import * as ContactsAPI from './utils/ContactsAPI'
 import ContactList from'./components/ContactList';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {contacts:[]};
+  }
+
+  componentDidMount(){
+    ContactsAPI.getAll().then((contacts) =>{
+      this.setState({contacts})
+    })
+  }
+
   render() {
     return (
       <div>
-        <ContactList />
+        <ContactList contacts={this.state.contacts}/>
       </div>
     );
   }
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,10 +1,50 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class ContactList extends Component{
+
+  static propTypes = {
+    contacts: PropTypes.array.isRequired
+  }
+
   render(){
+    const { contacts } = this.props
+
     return(
       <div>
-        Contact List
+        <div className="top-header">
+          <div className="wrap">
+            <div className="line-divider"></div>
+            <p className="title-text">Front-End Test</p>
+          </div>
+        </div>
+
+        <div className="wrap">
+          {contacts.map(contact =>
+            <div className="user-box" key={contact.id}>
+              <div>
+                <div className="user-avatar" style={{
+                  backgroundImage: `url(${contact.img})`}}></div>
+                <div className="user-info">
+                  <p className="user-title">{contact.name}</p>
+                  <div className="user-id">
+                    <p>id: {contact.id}</p>
+                    <p>{contact.username}</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="user-pay">
+                <span className="add-payment"></span>
+
+                <button >Pagar</button>
+
+              </div>
+
+            </div>
+          )}
+
+        </div>
       </div>
     );
   }
