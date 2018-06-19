1f39245125205941ad923ae4577a94a720f31ee6
Everton Boni <evertonferreira.de@gmail.com>
2018-06-19 19:09:51 -0300

    add new component ContactList

---
+++ src/components/ContactList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ContactList extends Component{
+  render(){
+    return(
+      <div>
+        Contact List
+      </div>
+    );
+  }
+}
+
+export default ContactList;
